967c30b58dbb740a7d98ba7e80072f1048f1d70a
LIMPIX31 <limpix31@gmail.com>
2023-04-24 16:51:36 +0200

    feat: init

---
+++ core/src/untitlence/vide/VideMain.kt
@@ -0,0 +1,18 @@
+package untitlence.vide
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.utils.ScreenUtils
+
+class VideMain : ApplicationAdapter() {
+  override fun create() {
+
+  }
+
+  override fun render() {
+    ScreenUtils.clear(1f, 0f, 0f, 1f)
+  }
+
+  override fun dispose() {
+
+  }
+}
